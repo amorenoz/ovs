a8c23af0b93563ba379710a1dbace80fc44c3960
Adrian Moreno <amorenoz@redhat.com>
2021-11-16 13:16:31 +0100

    python: detect changes in flow formatting code
    In order to minimize the risk of having the python flow parsing code and
    the C flow formatting code divert, add a target that checks if the
    formatting code has been changed since the last revision and warn the
    developer if it has.
    
    The script also makes it easy to update the dependency file so hopefully
    it will not cause too much trouble for a developer that has modifed the
    file without changing the flow string format.
    
    Signed-off-by: Adrian Moreno <amorenoz@redhat.com>

---
+++ python/ovs/flows/deps.py
@@ -0,0 +1,5 @@
+# File automatically generated. Do not modify manually!
+dependencies = {
+    "lib/ofp-actions.c": "f108b3e119f03b3373064589aecdeaf0",
+    "lib/odp-util.c": "4e2ce0d47bd82ebf50082a0fb2d390c5"
+}
