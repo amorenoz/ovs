0402a69b63653e5c0e912a0cf8ca20558cc20186
Adrian Moreno <amorenoz@redhat.com>
2021-11-16 17:11:02 +0100

    python: add unit tests for filtering engine
    Add unit test for OFFilter class.
    
    Signed-off-by: Adrian Moreno <amorenoz@redhat.com>

--- python/automake.mk
+++ python/automake.mk
@@ -54,6 +54,7 @@ ovs_pyfiles = \
 	python/ovs/winutils.py
 
 ovs_pytests = \
+	python/ovs/tests/test_filter.py \
 	python/ovs/tests/test_kv.py \
 	python/ovs/tests/test_list.py \
 	python/ovs/tests/test_odp.py \
