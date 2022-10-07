166fab7f2ca3c87b371e97cf22abecd976c1363d
Adrian Moreno <amorenoz@redhat.com>
2022-10-07 10:12:41 +0200

    TEST use pahole-geneated headers
    generated with:
    
     ~/code/dwarves/build/pahole -C ovs_barrier_impl --compile ovs-vswitchd_f35 >
     utilities/usdt-scripts/ovs_structs.h
    
     ~/code/dwarves/build/pahole -C udpif --compile ovs-vswitchd_f35 >>
     utilities/usdt-scripts/ovs_structs.h
    
    Signed-off-by: Adrian Moreno <amorenoz@redhat.com>

--- utilities/usdt-scripts/ovs_structs.h
+++ utilities/usdt-scripts/ovs_structs.h
@@ -1,123 +1,188 @@
-/*
- * Copyright (c) 2022 Red Hat, Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at:
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- *
- * The purpose of this include file is to define commonly used OVS data
- * structures so they can easily be used/accessed by BPF programs.
- */
-#ifndef OVS_STRUCTS_H
-#define OVS_STRUCTS_H 1
+typedef unsigned int __uint32_t;
+typedef __uint32_t uint32_t;
 
-/* From eBPF we do not care about atomic reading for now :) */
-#define ATOMIC(TYPE) TYPE
+typedef _Atomic unsigned int  atomic_uint;
+
+struct atomic_count {
+	atomic_uint                count;                /*     0     4 */
+
+	/* size: 4, cachelines: 1, members: 1 */
+	/* last cacheline: 4 bytes */
+};
+typedef struct atomic_count atomic_count;
+
+struct seq;
+
+struct ovs_refcount {
+	atomic_uint                count;                /*     0     4 */
+
+	/* size: 4, cachelines: 1, members: 1 */
+	/* last cacheline: 4 bytes */
+};
+
+struct ovs_barrier_impl {
+	uint32_t                   size;                 /*     0     4 */
+	atomic_count               count;                /*     4     4 */
+	struct seq *               seq;                  /*     8     8 */
+	struct ovs_refcount        refcnt;               /*    16     4 */
+
+	/* size: 24, cachelines: 1, members: 4 */
+	/* padding: 4 */
+	/* last cacheline: 24 bytes */
+};
 
 
-/* Various typedef's. */
-typedef uint32_t HANDLE;
+struct ovs_list;
+
+struct ovs_list {
+	struct ovs_list *          prev;                 /*     0     8 */
+	struct ovs_list *          next;                 /*     8     8 */
+
+	/* size: 16, cachelines: 1, members: 2 */
+	/* last cacheline: 16 bytes */
+};
+
+struct dpif;
+
+struct dpif_backer;
+
+struct handler;
+
+typedef unsigned int __uint32_t;
+typedef __uint32_t uint32_t;
+
+struct revalidator;
+
+struct latch {
+	int                        fds[2];               /*     0     8 */
+
+	/* size: 8, cachelines: 1, members: 1 */
+	/* last cacheline: 8 bytes */
+};
+
+struct seq;
+
+struct ovs_barrier_impl;
 
 
-/* The below we can not get from importing the pthread.h file as this will
- * clash with the Linux kernel includes done by BCC. */
-typedef struct pthread_mutex_s {
-    char size[OVS_PTHREAD_MUTEX_T_SIZE];
+struct ovs_barrier {
+	struct {
+		_Atomic struct ovs_barrier_impl *  p;    /*     0     8 */
+	} impl;                                          /*     0     8 */
+
+	/* size: 8, cachelines: 1, members: 1 */
+	/* last cacheline: 8 bytes */
+};
+
+struct dpif_flow_dump;
+
+typedef _Atomic _Bool  atomic_bool;
+
+
+
+struct umap;
+
+typedef _Atomic unsigned int  atomic_uint;
+
+typedef _Atomic long long int  atomic_llong;
+
+struct __pthread_internal_list;
+
+struct __pthread_internal_list {
+	struct __pthread_internal_list * __prev;         /*     0     8 */
+	struct __pthread_internal_list * __next;         /*     8     8 */
+
+	/* size: 16, cachelines: 1, members: 2 */
+	/* last cacheline: 16 bytes */
+};
+typedef struct __pthread_internal_list __pthread_list_t;
+
+struct __pthread_mutex_s {
+	int                        __lock;               /*     0     4 */
+	unsigned int               __count;              /*     4     4 */
+	int                        __owner;              /*     8     4 */
+	unsigned int               __nusers;             /*    12     4 */
+	int                        __kind;               /*    16     4 */
+	short int                  __spins;              /*    20     2 */
+	short int                  __elision;            /*    22     2 */
+	__pthread_list_t           __list;               /*    24    16 */
+
+	/* size: 40, cachelines: 1, members: 8 */
+	/* last cacheline: 40 bytes */
+};
+
+typedef union {
+	struct __pthread_mutex_s   __data;             /*     0    40 */
+	char                       __size[40];         /*     0    40 */
+	long int                   __align;            /*     0     8 */
 } pthread_mutex_t;
 
-
-/* Included from lib/ovs-rcu.h */
-#define OVSRCU_TYPE(TYPE) struct { ATOMIC(TYPE) p; }
-
-
-/* Included from lib/ovs-atomic.h */
-typedef ATOMIC(bool)               atomic_bool;
-typedef ATOMIC(int)                atomic_int;
-typedef ATOMIC(unsigned int)       atomic_uint;
-typedef ATOMIC(long long)          atomic_llong;
-typedef ATOMIC(unsigned long long) atomic_ullong;
-
-
-/* Included from include/openvswitch/list.h */
-struct ovs_list {
-    struct ovs_list *prev;
-    struct ovs_list *next;
-};
-
-
-/* Included from lib/latch.h */
-struct latch {
-    HANDLE wevent;
-    bool is_set;
-};
-
-/* Included from lib/ovs-thread.h */
-struct ovs_barrier_impl;
-struct ovs_barrier {
-    OVSRCU_TYPE(struct ovs_barrier_impl *) impl;
-};
-
-
-/* Included from include/openvswitch/thread.h */
 struct ovs_mutex {
-    pthread_mutex_t lock;
-    const char *where;
+	pthread_mutex_t            lock;                 /*     0    40 */
+	const char  *              where;                /*    40     8 */
+
+	/* size: 48, cachelines: 1, members: 2 */
+	/* last cacheline: 48 bytes */
 };
 
+struct unixctl_conn;
 
-/* Include from ofproto/ofproto-dpif.c */
+typedef long unsigned int __uint64_t;
+typedef __uint64_t uint64_t;
+
+typedef long unsigned int size_t;
+
 struct udpif {
-    struct ovs_list list_node;
+	struct ovs_list            list_node;            /*     0    16 */
+	struct dpif *              dpif;                 /*    16     8 */
+	struct dpif_backer *       backer;               /*    24     8 */
+	struct handler *           handlers;             /*    32     8 */
+	uint32_t                   n_handlers;           /*    40     4 */
 
-    struct dpif *dpif;
-    struct dpif_backer *backer;
+	/* XXX 4 bytes hole, try to pack */
 
-    struct handler *handlers;
-    uint32_t n_handlers;
+	struct revalidator *       revalidators;         /*    48     8 */
+	uint32_t                   n_revalidators;       /*    56     4 */
+	struct latch               exit_latch;           /*    60     8 */
 
-    struct revalidator *revalidators;
-    uint32_t n_revalidators;
+	/* XXX 4 bytes hole, try to pack */
 
-    struct latch exit_latch;
+	/* --- cacheline 1 boundary (64 bytes) was 8 bytes ago --- */
+	struct seq *               reval_seq;            /*    72     8 */
+	_Bool                      reval_exit;           /*    80     1 */
 
-    struct seq *reval_seq;
-    bool reval_exit;
-    struct ovs_barrier reval_barrier;
-    struct dpif_flow_dump *dump;
-    long long int dump_duration;
-    struct seq *dump_seq;
-    atomic_bool enable_ufid;
+	/* XXX 7 bytes hole, try to pack */
 
-    bool pause;
-    struct latch pause_latch;
-    struct ovs_barrier pause_barrier;
+	struct ovs_barrier         reval_barrier;        /*    88     8 */
+	struct dpif_flow_dump *    dump;                 /*    96     8 */
+	long long int              dump_duration;        /*   104     8 */
+	struct seq *               dump_seq;             /*   112     8 */
+	atomic_bool                enable_ufid;          /*   120     1 */
+	_Bool                      pause;                /*   121     1 */
 
-    struct umap *ukeys;
+	/* XXX 2 bytes hole, try to pack */
 
-    unsigned int max_n_flows;
-    unsigned int avg_n_flows;
+	struct latch               pause_latch;          /*   124     8 */
 
-    atomic_uint flow_limit;
+	/* XXX 4 bytes hole, try to pack */
 
-    atomic_uint n_flows;
-    atomic_llong n_flows_timestamp;
-    struct ovs_mutex n_flows_mutex;
+	/* --- cacheline 2 boundary (128 bytes) was 8 bytes ago --- */
+	struct ovs_barrier         pause_barrier;        /*   136     8 */
+	struct umap *              ukeys;                /*   144     8 */
+	unsigned int               max_n_flows;          /*   152     4 */
+	unsigned int               avg_n_flows;          /*   156     4 */
+	atomic_uint                flow_limit;           /*   160     4 */
+	atomic_uint                n_flows;              /*   164     4 */
+	atomic_llong               n_flows_timestamp;    /*   168     8 */
+	struct ovs_mutex           n_flows_mutex;        /*   176    48 */
+	/* --- cacheline 3 boundary (192 bytes) was 32 bytes ago --- */
+	struct unixctl_conn * *    conns;                /*   224     8 */
+	uint64_t                   conn_seq;             /*   232     8 */
+	size_t                     n_conns;              /*   240     8 */
+	long long int              offload_rebalance_time; /*   248     8 */
 
-    struct unixctl_conn **conns;
-    uint64_t conn_seq;
-    size_t n_conns;
-
-    long long int offload_rebalance_time;
+	/* size: 256, cachelines: 4, members: 29 */
+	/* sum members: 235, holes: 5, sum holes: 21 */
 };
 
 
-#endif /* OVS_STRUCTS_H */
